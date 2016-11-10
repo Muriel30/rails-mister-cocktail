944e8e48e124bc30283cef610c6bd2164ef02356
Muriel Berthou Crestey <murielberthoucrestey@gmail.com>
2016-11-10 12:30:50 +0100

    Prepare rails app with external specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,45 @@
 Rails.application.routes.draw do
+  get 'dose/index'
+
+  get 'dose/show'
+
+  get 'dose/new'
+
+  get 'dose/create'
+
+  get 'dose/edit'
+
+  get 'dose/update'
+
+  get 'dose/destroy'
+
+  get 'ingredient/index'
+
+  get 'ingredient/show'
+
+  get 'ingredient/new'
+
+  get 'ingredient/create'
+
+  get 'ingredient/edit'
+
+  get 'ingredient/update'
+
+  get 'ingredient/destroy'
+
+  get 'cocktail/index'
+
+  get 'cocktail/show'
+
+  get 'cocktail/new'
+
+  get 'cocktail/create'
+
+  get 'cocktail/edit'
+
+  get 'cocktail/update'
+
+  get 'cocktail/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
